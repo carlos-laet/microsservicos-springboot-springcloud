9f511100b3ea835c499988e9efeb2166f02f13fa
carlos-laet <carlosedulaet@hotmail.com>
2022-03-19 22:06:15 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/laetprojetos/hrworker/entities/Worker.java
@@ -0,0 +1,22 @@
+package com.laetprojetos.hrworker.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_worker")
+public class Worker implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+}
